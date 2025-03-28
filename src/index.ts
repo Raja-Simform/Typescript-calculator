aa6961802b4337778bb7ba30c9ee84067266bf78
Raja-Simform <raja.jha@simformsolutions.com>
2025-03-28 14:17:19 +0530

    feat:add module file

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// console.log('hello world whatapp');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const element = document.getElementById('class');
+//   if (element) {
+//     element.innerHTML = 'Hello, World!';
+//   } else {
+//     console.error('Element with ID "class" not found.');
+//   }
+// });
+// let name=10;
